967c54f0c134e4c16029e08a30d795dc5f97e9ac
Elise Miller <elisemiller@campus-112-074.ucdavis.edu>
2021-10-14 14:52:55 -0700

    added classmate responses from class and commenting alternative methods for doing the same thing

--- week_3_homework.R
+++ week_3_homework.R
@@ -4,14 +4,23 @@ surveys <- read.csv(file = 'data/portal_data_joined.csv')
 surveys_base <- select(surveys, species_id, weight, plot_type)
 #or surveys_base <- surveys[c('species_id', 'weight','plot_type')]
 surveys_base <- surveys_base[1:60,]
+#or surveys_base <- head(x = surveys_base,60)
+#or to combine these two lines of code:
+#surveys_base <- surveys[c(1:60),c(6,9,13)] or you can call them by name
 
 surveys_base$species_id <- as_factor(surveys_base$species_id)
 surveys_base$plot_type <- as_factor(surveys_base$plot_type)
+class(surveys_base$species_id)
 
 #a factor is grouped into different categories or levels. It is an organized system rather than just a group of different things.
 View(surveys_base)
-no_na_surveys_base <- na.omit(surveys_base)
+#no_na_surveys_base <- na.omit(surveys_base) #assumes only NAs are weight
+#instead try
+no_na_surveys_base <- surveys_base %>% na.omit(surveys_base$weight)
+#or na.omit(surveys_base, surveys_base$weight)
 View(no_na_surveys_base)
+#or surveys_base[is.ne(surveys_base$weight),]
+#or surveys_base[!is.na(surveys_base$weight),]
 
 challenge_base <- filter(no_na_surveys_base, weight >= 150)
 View(challenge_base)
